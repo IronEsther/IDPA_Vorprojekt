474c9c7bb4b8f3f1c80b86005ddc0e02e5791abf
Marmeladenbrot1 <N.goehl.inf21@stud.bbbaden.ch>
2023-08-30 15:34:39 +0200

    Eingabeverarbeitung
    Eingabeverarbeitung

--- IDPA_Vorprojekt_SAND/Program.cs
+++ IDPA_Vorprojekt_SAND/Program.cs
@@ -5,9 +5,28 @@ namespace IDPA_Vorprojekt_SAND
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            
+        }
 
+        static double Eingabe()
+        {
+            string input;
+            double output = 0;
+            while (output == 0)
+            {
+                try
+                {
+                    input = Console.ReadLine();
+                    output = Convert.ToDouble(input);
+                }
+                catch
+                {
+                    Console.WriteLine("Es werden nur Zahlen aktzeptieren. Gib bitte eine gültige Eingabe ein.");
+                }
+            }
+            return output;
         }
     }
 }
