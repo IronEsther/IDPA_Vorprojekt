5f386ac78c41e201dace87669d5255e3a25f3554
Marmeladenbrot1 <N.goehl.inf21@stud.bbbaden.ch>
2023-08-30 16:15:23 +0200

    Hauptteil
    Hauptteil ist fertig, ein paar Fehlerbehandlungen fehlen noch

--- IDPA_Vorprojekt_SAND/Program.cs
+++ IDPA_Vorprojekt_SAND/Program.cs
@@ -8,7 +8,38 @@ namespace IDPA_Vorprojekt_SAND
     {
         static void Main()
         {
-            
+            double Kapital;
+            double Geburtstag;
+            double Zinssatz;
+            double Bonus;
+            while (true)
+            {
+                Tastatur("Geben Sie bitte dïe Höhe des Kapitals ein.");
+                Console.WriteLine("Zum Beispiel: Falls Sie ein Kapital in Höhe von 203'495,55 haben dann geben Sie bitte 203495.55 ein.");
+                Kapital = Eingabe();
+                Tastatur("Geben Sie bitte den Tag an, an dem der Kunde Geburtstag hat");
+                Console.WriteLine("Zum Beispiel: Falls Herr Müller am 21.06.2000 geboren ist ist nur 21 als Eingabe benötigt.");
+                Geburtstag = Eingabe();
+                Tastatur("Geben Sie den Spezialzinssatz an.");
+                Console.WriteLine("Zum Beispiel: Falls Sie einen Zins von 2,1% eingeben wollen, dann geben Sie 2.1 ein.");
+                Zinssatz = Eingabe();
+                if (Geburtstag > 30)
+                {
+                    Geburtstag = 30;
+                }
+                else if(Geburtstag < 1)
+                {
+                    Geburtstag = 1;
+                }
+                Bonus = Kapital * Zinssatz / 100 / 360 * Geburtstag;
+                Tastatur("Der Bonus für den Kunden beträgt " + Bonus + " CHF.");
+                Tastatur("Wollen Sie noch einen Kunden erfassen? [Ja/Nein]");
+                if(Console.ReadLine() == "Nein")
+                {
+                    Environment.Exit(0);
+                }
+
+            }
         }
 
         static double Eingabe()
